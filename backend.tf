3749b461145ab13f324b214509acc546293e9ad7
Nikhil <nikhilvadla500@gmail.com>
2025-10-27 17:38:16 +0530

    modified

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "nikhil-terraform" # GCS bucket name
+    bucket = "bucket-nikhil-0123" # GCS bucket name
     prefix = "terraform/state" # Path within the bucket (e.g., a folder structure)
   }
 }
